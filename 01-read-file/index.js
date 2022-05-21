acb91c77e4d14a888603b1e86e8bdf6f2fe59ac3
Rustam <tabarzin@gmail.com>
2022-05-21 15:35:43 +0300

    feat: finish first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');    
+const filePath = path.join(__dirname, 'text.txt');
+
+const reader = fs.createReadStream(filePath);
+
+reader.on('data', (data) => {
+console.log(data.toString());
+});
+
+
