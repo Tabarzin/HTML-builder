c54db35d01edcb9016dd8826a80ee7a3afe7e67c
Rustam <tabarzin@gmail.com>
2022-05-23 00:31:05 +0300

    feat: add copy code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require("path");
+const destPath = path.join(__dirname, "files-copy");
+const srcPath = path.join(__dirname, "files");
+const fs = require("fs");
+const fsPromises = fs.promises;
 
+fsPromises.mkdir(destPath, { recursive: true });
+
+fs.readdir(srcPath, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      let filePath = path.join(srcPath, file);
+      let fileCopyPath = path.join(destPath, file);
+
+      fs.copyFile(filePath, fileCopyPath, (err) => {
+        if (err) {
+          console.error("Sorry, I failed to copy ", err);
+          return;
+        }
+        console.log("Files successfuly copied");
+      });
+    });
+  }
+});
