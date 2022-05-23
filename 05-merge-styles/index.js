e970ae1348cd8bc5d0b0ef7f35b355c7ec9e33ca
Rustam <tabarzin@gmail.com>
2022-05-24 00:53:08 +0300

    feat: add code

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const fs = require("fs");
+const srcPath = path.join(__dirname, "styles");
+const destPath = path.join(__dirname, "project-dist");
+
+const fsPromises = fs.promises;
+
+fs.readdir(srcPath, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      let filePath = path.join(srcPath, file);
+
+      fs.stat(filePath, (error, stats) => {
+        if (stats.isFile() && path.extname(file) == ".css") {
+          const reader = fs.createReadStream(filePath);
+
+          reader.on("data", (data) => {
+            fs.appendFile(
+              "05-merge-styles/project-dist/bundle.css",
+              data.toString(),
+              (err) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log("Success!");
+                }
+              }
+            );
+          });
+        } else {
+          return;
+        }
+      });
+    });
+  }
+});
