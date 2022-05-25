70f9ead64f5d4021cb041703dbe8f5bd363991c7
Rustam <tabarzin@gmail.com>
2022-05-25 18:18:48 +0300

    fix: add folder update

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,25 +1,42 @@
-const path = require('path');
-const destPath = path.join(__dirname, 'files-copy');
-const srcPath = path.join(__dirname, 'files');
-const fs = require('fs');
+const path = require("path");
+const destPath = path.join(__dirname, "files-copy");
+const srcPath = path.join(__dirname, "files");
+const fs = require("fs");
+const { createDecipheriv } = require("crypto");
 const fsPromises = fs.promises;
 
-fsPromises.mkdir(destPath, { recursive: true });
+fs.access("04-copy-directory/files-copy", (err) => {
+  if (err) {
+    createDir();
+  } else {
+    fs.rm("04-copy-directory/files-copy", { recursive: true }, (err) => {
+      if (err) {
+        return console.log("error occurred in deleting directory");
+      }
 
-fs.readdir(srcPath, (err, files) => {
-  if (err) console.log(err);
-  else {
-    files.forEach((file) => {
-      let filePath = path.join(srcPath, file);
-      let fileCopyPath = path.join(destPath, file);
-
-      fs.copyFile(filePath, fileCopyPath, (err) => {
-        if (err) {
-          console.error('Sorry, I failed to copy ', err);
-          return;
-        }
-        console.log('File successfuly copied');
-      });
+      createDir();
     });
   }
 });
+
+function createDir() {
+  fsPromises.mkdir(destPath, { recursive: true });
+
+  fs.readdir(srcPath, (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        let filePath = path.join(srcPath, file);
+        let fileCopyPath = path.join(destPath, file);
+
+        fs.copyFile(filePath, fileCopyPath, (err) => {
+          if (err) {
+            console.error("Sorry, I failed to copy ", err);
+            return;
+          }
+          console.log("File successfuly copied");
+        });
+      });
+    }
+  });
+}
